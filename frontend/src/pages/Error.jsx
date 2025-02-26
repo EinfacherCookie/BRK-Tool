f5599a1e4c0dd261d105614b381d9be568f809f6
Moritz <cookie.einfacher@gmail.com>
2025-02-26 22:30:23 +0100

    Test Commit

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Error = () => {
-	return <div>Error, this Site does not exist!</div>;
+	return <div>Error, this Site does not exist! 404</div>;
 };
 
 export default Error;
